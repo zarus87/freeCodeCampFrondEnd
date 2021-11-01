828e24bdf37954c7a5885b57a7a90b3ea897f676
Kirill <86402812+zarus87@users.noreply.github.com>
2021-11-01 15:58:22 +0300

    react 16

--- freeCodeCampFrond.jsx
+++ freeCodeCampFrond.jsx
@@ -218,4 +218,32 @@
   ReactDOM.render(<MyComponent/>, document.getElementById("challenge-node"));
 
 
-16.       
+16.     const CurrentDate = (props) => {
+  return (
+    <div>
+      { /* Change code below this line */ }
+      <p>The current date is:{props.date} </p>    //какое-то свойство добавляю
+      { /* Change code above this line */ }
+    </div>
+  );
+};
+
+class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>What date is it?</h3>
+
+        { /* Change code below this line */ }
+        <CurrentDate date={Date()}/>                          // здесь тоже
+        { /* Change code above this line */ }
+      </div>
+    );
+  }
+};  
+
+
+17.       
